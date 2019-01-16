b06793484cf185d597c9de6e980bc5af8691b8cc
phuduc <phuduc212@gmail.com>
2019-01-16 11:21:59 +0700

    redirect URI

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-// add this file to .gitignore
-module.exports = {
-    google: {
-        clientID: '105629163278-ghq3m78c43640jm0is3fkj7mfmnc1q51.apps.googleusercontent.com',
-        clientSecret: 'zCgIVLWL3rkmv7ZrAsznuuTM'
-    }
-}
--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,14 +1,14 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
-const keys = require('./keys');
+const keys = require('../keys');
 
 passport.use(
     new GoogleStrategy({
-    // options for the strategy
+        // options for google strategy
         clientID: keys.google.clientID,
-        clientSecret: keys.google.clientSecret
-    }),
-    () => {
+        clientSecret: keys.google.clientSecret,
+        callbackURL: '/auth/google/redirect'
+    }, () => {
         // passport callback function
-    }
+    })
 )
