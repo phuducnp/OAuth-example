68d3780c1606342238c630941bcde7e6980f218d
phuduc <phuduc212@gmail.com>
2019-01-16 10:24:24 +0700

    googleStrategy setup and securing keys

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// add this file to .gitignore
+module.exports = {
+    google: {
+        clientID: '105629163278-ghq3m78c43640jm0is3fkj7mfmnc1q51.apps.googleusercontent.com',
+        clientSecret: 'zCgIVLWL3rkmv7ZrAsznuuTM'
+    }
+}
