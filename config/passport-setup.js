547b9778a06da62c3e8148c49893aa2db307396b
phuduc <phuduc212@gmail.com>
2019-01-18 15:25:38 +0700

    setup mongo and save user to db

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('../keys');
+const User = require('../models/user-model');
 
 passport.use(
     new GoogleStrategy({
@@ -10,7 +11,24 @@ passport.use(
         callbackURL: '/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
         // passport callback function
-        console.log('passport callback function fire');
-        console.log(profile);
+        // console.log('passport callback function fire');
+        // console.log(profile);
+        // check if user exist in db
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser) {
+                // already have user
+                console.log('user is: ', currentUser);
+            } else {
+                // if not, create user
+                new User({
+                    userName: profile.displayName,
+                    googleId: profile.id
+                }).save().then((newUser) => {
+                    console.log('New user created' + newUser);
+                });
+            }
+        });
+
+        
     })
 )
